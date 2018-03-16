49bbdadc76ef6f69139e268c749f9f32740352ef
Sup <Furkan Taha NDER>
2018-03-16 23:05:26 +0300

    Yeni controllerslar eklendi ve modeller eklendi

--- src/application/controllers/Post_Controller.php
+++ src/application/controllers/Post_Controller.php
@@ -42,7 +42,155 @@ class Post_Controller extends CI_Controller{
                  
         $return = $this->generalChangeProcess_model->deleteTable('post',$data);
         
-    }     
+    } 
 
+    public function getAllPosts(){
+    	$data = $this->post_model->getAllPosts();
+    	$HTML = "";
+
+    	foreach ($data as $row) {
+    		$HTML .= "<tr role=\"row\" class=\"odd\">
+			    		<td class=\"sorting_1\">" .$row[id]."</td>
+			    		<td> ". $row["subject"] ."</td>
+			    		<td> ". $row["created"] ."</td>
+			    		<td> ". $row["thread_id"] ."</td>
+			    		<td> ". $row["user_account_id"] ."</td>
+			    		<td> ". $row["postStatus"] ."</td>
+    		    	</tr> ";
+
+    	}
+    	echo $HTML;
+    } 
+
+    public function getPostbyCategory($categoryID){
+    	$data = $this->post_model->getPostbyCategory($categoryID);
+    	$HTML = "";
+    	
+    	foreach ($data as $row) {
+    		$HTML .= "<tr role=\"row\" class=\"odd\">
+			    		<td class=\"sorting_1\">" .$row[id]."</td>
+			    		<td> ". $row["name"] ."</td>
+			    		<td> ". $row["description"] ."</td>
+			    		<td> ". $row["creator"] ."</td>
+			    		<td> ". $row["created"] ."</td>
+			    		<td> ". $row["category_status"] ."</td>
+    		    	</tr> ";
+
+    	}
+    	echo $HTML;
+    }   
+
+    public function getAllCategoryList(){
+    	$data = $this->post_model->getAllCategoryList();
+    	$HTML = "";
+
+    	foreach ($data as $row) {
+    		$HTML .= "<tr role=\"row\" class=\"odd\">
+			    		<td class=\"sorting_1\">" .$row[id]."</td>
+			    		<td> ". $row["name"] ."</td>
+			    		<td> ". $row["description"] ."</td>
+			    		<td> ". $row["creator"] ."</td>
+			    		<td> ". $row["created"] ."</td>
+			    		<td> ". $row["categoryID"] ."</td>
+			    		<td> ". $row["category_status"] ."</td>
+    		    	</tr> ";
+
+    	}
+    	echo $HTML;
+    }
+
+    public function getAllThreads(){
+    	$data = $this->post_model->getAllThreads();
+    	$HTML = "";
+
+    	foreach ($data as $row) {
+    		$HTML .= "<tr role=\"row\" class=\"odd\">
+			    		<td class=\"sorting_1\">" .$row[id]."</td>
+			    		<td> ". $row["subject"] ."</td>
+			    		<td> ". $row["created"] ."</td>
+			    		<td> ". $row["userAccountID"] ."</td>
+			    		<td> ". $row["threadStatus"] ."</td>
+			    		<td> ". $row["status"] ."</td>
+			    		<td> ". $row["category_id"] ."</td>
+    		    	</tr> ";
+
+    	}
+    	echo $HTML;
+    }
+
+     public function getAllThreadsByCategory($CategoryID){
+    	$data = $this->post_model->getAllThreadsByCategory($CategoryID);
+    	$HTML = "";
+
+    	foreach ($data as $row) {
+    		$HTML .= "<tr role=\"row\" class=\"odd\">
+			    		<td class=\"sorting_1\">" .$row[id]."</td>
+			    		<td> ". $row["subject"] ."</td>
+			    		<td> ". $row["created"] ."</td>
+			    		<td> ". $row["userAccountID"] ."</td>
+			    		<td> ". $row["threadStatus"] ."</td>
+			    		<td> ". $row["status"] ."</td>
+    		    	</tr> ";
+
+    	}
+    	echo $HTML;
+    }
+
+    public function getAllThreadsByCategoryIrreveland(){
+    	$data = $this->post_model->getAllThreadsByCategoryIrreveland();
+    	$HTML = "";
+
+    	foreach ($data as $row) {
+    		$HTML .= "<tr role=\"row\" class=\"odd\">
+			    		<td class=\"sorting_1\">" .$row[id]."</td>
+			    		<td> ". $row["subject"] ."</td>
+			    		<td> ". $row["created"] ."</td>
+			    		<td> ". $row["userAccountID"] ."</td>
+			    		<td> ". $row["status"] ."</td>
+    		    	</tr> ";
+
+    	}
+    	echo $HTML;
+    }
 } 
+
+class post_model extends CI_Model{
+
+	public function getAllPosts(){
+		$SQL ="";
+		$query = $this->db->query($_SQL);
+		return $query->result_array();
+	}	
+
+	public function getPostbyCategory($categoryID){
+		$SQL ="";
+		$query = $this->db->query($_SQL);
+		return $query->result_array();
+	}
+
+	public function getAllCategoryList(){
+		$SQL ="";
+		$query = $this->db->query($_SQL);
+		return $query->result_array();
+	}
+
+    public function getAllThreads(){
+    	$SQL ="";
+		$query = $this->db->query($_SQL);
+		return $query->result_array();
+	}
+    
+    public function getAllThreadsByCategory($CategoryID){
+		$SQL ="";
+		$query = $this->db->query($_SQL);
+		return $query->result_array();
+	}
+
+	public function getAllThreadsByCategoryIrreveland(){
+		$SQL ="";
+		$query = $this->db->query($_SQL);
+		return $query->result_array();
+	}	
+}
+
 ?>
