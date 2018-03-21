f877adbef858cd188f2403c7e8a3b3f73592036a
Pakize Nur Ates <pakizenurates@gmail.com>
2018-03-21 08:00:40 -0700

    PostGirdi_Controller olusturuldu

--- src/application/controllers/Post_Controller.php
+++ src/application/controllers/Post_Controller.php
@@ -8,42 +8,6 @@ class Post_Controller extends CI_Controller{
        $this->load->view('_footer');
     }
 
-    public function insertPost(){
-        $data = array();
-        
-        $data["subject"] = $this->input->post("subject");
-        $data["content"] = $this->input->post("content");
-        $data["created"] = $date('Y-m-d H:i:s');
-        
-        if($this->input>-post->["thread_id"] != NULL ){
-            $data["thread_id"] = $this->input->post("thread_id");
-        }
-        
-        $data["postStatus"] = 1;
-        $data["insert_date"] = $date('Y-m-d H:i:s');
-
-        $return = $this->generalChangeProcess_model->insertTables('post',$data);
-            
-    }
-
-    public function updatePost(){
-        $data = array();
-
-        $data["subject"] = $this->input->post("subject");
-        $data["content"] = $this->input->post("content");
-        $data["update_date"] = $date('Y-m-d H:i:s');
-
-        $return = $this->generalChangeProcess_model->updateTable('post',$data);
-
-    }    
- 
-    public function deletePost(){
-        $data = array();
-                 
-        $return = $this->generalChangeProcess_model->deleteTable('post',$data);
-        
-    } 
-
     public function getAllPosts(){
     	$data = $this->post_model->getAllPosts();
     	$HTML = "";
